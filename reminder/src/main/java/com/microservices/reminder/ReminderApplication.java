0d02975a4dd9b31e135a6feb350a3ddc21b5e26c
Mayconn Cardoso Soares <mayconncardoso2015@gmail.com>
2024-02-12 19:51:46 -0300

    initial config

---
+++ reminder/src/main/java/com/microservices/reminder/ReminderApplication.java
@@ -0,0 +1,15 @@
+package com.microservices.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class ReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderApplication.class, args);
+	}
+
+}
